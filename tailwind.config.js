ce414bb31b7501ca9826bcbff8061405b566c398
Okyan Awang Ramadhana <okyanawang@gmail.com>
2022-09-29 22:35:02 +0700

    Going to finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
       colors:{
         primary: '#14b8a6',
         secondary: '#64748b',
-        dark: '#0f172a'
+        dark: '#0f172a',
+        tourist: '#a5f3fc'
       },
       screens: {
         '2xl' : '1320px'
